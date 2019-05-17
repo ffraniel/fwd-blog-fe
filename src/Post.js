7ba6dccc148851de576b6385bf715103a86b4668
ffraniel <francis.whitehead@googlemail.com>
2019-05-17 18:29:35 +0100

    added starter plain text creator

--- src/Post.js
+++ src/Post.js
@@ -20,12 +20,31 @@ const Post = (props) => {
     getArticle();
   }, [props.match.params.postSlug]);
 
+  function toPlainText(blocks = []) {
+    //  tweak this
+    return blocks
+      // loop through each block
+      .map(block => {
+        // if it's not a text block with children, 
+        // return nothing
+        if (block._type !== 'block' || !block.children) {
+          return ''
+        }
+        // loop through the children spans, and join the
+        // text strings
+        return block.children.map(child => child.text).join('')
+      })
+      // join the parapgraphs leaving split by two linebreaks
+      .join('\n\n')
+  };
+
   return (
     <section className="post">
       {articleLoading && <h1>Loading</h1>}
       {!articleLoading && 
         <article className="article">
-          <h3>{article.title}</h3>    
+          <h3>{article.title}</h3>  
+          {toPlainText(article.body)}  
         </article>}
     </section>
   )
