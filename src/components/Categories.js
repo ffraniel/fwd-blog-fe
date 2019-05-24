8232489bc012618155749ac3987e023496f4fcf0
ffraniel <francis.whitehead@googlemail.com>
2019-05-24 16:08:27 +0100

    footer styling

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 const Categories = (props) => {
   const { categories } = props;
   return (
-    <section className="categories-list">
+    <section className="categories-footer-list">
       {categories.map(category => {
         return (
           <ul key={category._id}>
-            <Link to={`category/${category.title}`} >{category.title}</Link>
+            <Link className="category-footer-list-item" to={`category/${category.title}`} >{category.title}</Link>
           </ul>
         );
       })}
